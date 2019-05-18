48af054e6f3b6e469bcd6ae948d219c268c94379
ChangHwan Kim <kimbob@kaist.ac.kr>
2019-05-19 03:14:44 +0900

    Merger for batch data in data_batch directory

---
+++ datasets/batch_merger.py
@@ -0,0 +1,31 @@
+import json
+import sys
+
+if __name__ == '__main__':
+    user_batch = {}
+    # batch for userlist 2
+    for i in range(12):
+        batch_file = '../data_batch/batch2_' + str(i) + '.json'
+        with open(batch_file, 'r') as fp:
+            per_user_batch = json.load(fp)
+
+        user_batch.update(per_user_batch)
+
+    # batch for userlist 3
+    for i in range(4):
+        batch_file = '../data_batch/batch3_' + str(i) + '.json'
+        with open(batch_file, 'r') as fp:
+            per_user_batch = json.load(fp)
+
+        user_batch.update(per_user_batch)
+
+    # batch for userlist 4
+    for i in range(3):
+        batch_file = '../data_batch/batch4_' + str(i) + '.json'
+        with open(batch_file, 'r') as fp:
+            per_user_batch = json.load(fp)
+
+        user_batch.update(per_user_batch)
+
+    with open('./userbatch.json', 'w') as fp:
+        json.dump(user_batch, fp)
