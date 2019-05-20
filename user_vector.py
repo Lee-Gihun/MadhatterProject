726d3917e8eea5f9f7ca1a5a2acd64fc8d0a36a7
ChangHwan Kim <kimbob@kaist.ac.kr>
2019-05-20 23:59:14 +0900

    Add TF-IDF with global win rate mode for user vector generation
    - mode 3 is TF-IDF with global win rate mode
    - user_vector scripts now read userbatch.json, which contains whole user
      data

--- user_vector.py
+++ user_vector.py
@@ -1,6 +1,7 @@
 import json
 from make_idf_table import make_idf_table
 from champ_id_remap import champ_id_remap
+from global_win_rate import global_win_rate
 import sys
 
 class UserVectorGenerator():
@@ -9,13 +10,64 @@ class UserVectorGenerator():
         mode: Determine which method to generate user vectors
             1: Naive method. Just get literal value from data
             2: TF-IDF method. Calcuate TF-IDF value for each vector value
+            3: TF-IDF with global win rate mode
         """
         self.remapped_champ_id = champ_id_remap()
         self.champ_num = len(self.remapped_champ_id)
+        self.global_win_rate = global_win_rate()
         self.mode = mode
 
         with open(batch_file, 'r') as fp:
             self.batch_data = json.load(fp)
+
+    def _get_user_vector_measure(self, user_name, champ_history, champ_idx, mode, measure):
+        """
+        user_name: User name
+            Needed for max_play_count
+        champ_history: Each champion history for user_name
+            Needed for getting base value such as play count, win_rate, championPoints
+        champ_idx: Remapped champion index
+            Needed for accessing idf_table
+        mode: Method for generating user vector measure
+        measure: Determine which measure to get compute the value of user vector
+            1: play count
+            2: mastery score
+        """
+        if measure == 1: # play count
+            if mode == 1: # Naive mode
+                return champ_history['play_count']
+
+            elif mode == 2: # TF-IDF mode
+                max_play_count = self._get_max_play_count(user_name)
+                return (champ_history['play_count'] / max_play_count) * idf_table[champ_idx]
+
+            elif mode == 3: # TF-IDF with global win rate
+                max_play_count = self._get_max_play_count(user_name)
+                this_champ_global_win_rate = self.global_win_rate[champ_idx]
+                this_user_champ_win_rate = champ_history['win_rate']
+
+                weighted_rate = this_user_champ_win_rate / this_champ_global_win_rate
+                play_count_tf_idf = (champ_history['play_count'] / max_play_count) * idf_table[champ_idx]
+
+                return weighted_rate * play_count_tf_idf
+
+        elif measure == 2: # mastery score
+            if mode == 1: # Naive mode
+                return champ_history['championPoints']
+
+            elif mode == 2: # TF-IDF mode
+                max_mastery_score = self._get_max_mastery_score(user_name)
+                return (champ_history['championPoints'] / max_mastery_score) * idf_table[champ_idx]
+
+            elif mode == 3: # TF-IDF with global win rate
+                max_mastery_score = self._get_max_mastery_score(user_name)
+                this_champ_global_win_rate = self.global_win_rate[champ_idx]
+                this_user_champ_win_rate = champ_history['win_rate']
+
+                weighted_rate = this_user_champ_win_rate / this_champ_global_win_rate
+                mastery_tf_idf = (champ_history['championPoints'] / max_mastery_score) * idf_table[champ_idx]
+
+                return weighted_rate * mastery_tf_idf
 
     def _get_max_play_count(self, user_name):
         """
@@ -33,21 +85,11 @@ class UserVectorGenerator():
     def _get_play_count_vector(self, user_name):
         play_count_vector = [0 for _ in range(self.champ_num)]
 
-        if self.mode == 1: # Naive mode
-            for champ_history in self.batch_data[user_name]['champion_history']:
-                original_champ_id = champ_history['champion_key']
-                champ_idx = self.remapped_champ_id[original_champ_id]
+        for champ_history in self.batch_data[user_name]['champion_history']:
+            original_champ_id = champ_history['champion_key']
+            champ_idx = self.remapped_champ_id[original_champ_id]
 
-                play_count_vector[champ_idx] = champ_history['play_count']
-        elif self.mode == 2: # TF-IDF mode
-            max_play_count = self._get_max_play_count(user_name)
-
-            for champ_history in self.batch_data[user_name]['champion_history']:
-                original_champ_id = champ_history['champion_key']
-                champ_idx = self.remapped_champ_id[original_champ_id]
-                play_count_tf_idf = (champ_history['play_count'] / max_play_count) * idf_table[champ_idx]
-
-                play_count_vector[champ_idx] = play_count_tf_idf
+            play_count_vector[champ_idx] = self._get_user_vector_measure(user_name, champ_history, champ_idx, self.mode, 1)
 
         return play_count_vector
 
@@ -67,21 +109,11 @@ class UserVectorGenerator():
     def _get_mastery_score_vector(self, user_name):
         mastery_score_vector = [0 for _ in range(self.champ_num)]
         
-        if self.mode == 1: # Naive mode
-            for champ_history in self.batch_data[user_name]['champion_history']:
-                original_champ_id = champ_history['champion_key']
-                champ_idx = self.remapped_champ_id[original_champ_id]
+        for champ_history in self.batch_data[user_name]['champion_history']:
+            original_champ_id = champ_history['champion_key']
+            champ_idx = self.remapped_champ_id[original_champ_id]
 
-                mastery_score_vector[champ_idx] = champ_history['championPoints']
-        elif self.mode == 2: # TF-IDF mode
-            max_mastery_score = self._get_max_mastery_score(user_name)
-
-            for champ_history in self.batch_data[user_name]['champion_history']:
-                original_champ_id = champ_history['champion_key']
-                champ_idx = self.remapped_champ_id[original_champ_id]
-                mastery_tf_idf = (champ_history['championPoints'] / max_mastery_score) * idf_table[champ_idx]
-
-                mastery_score_vector[champ_idx] = mastery_tf_idf
+            mastery_score_vector[champ_idx] = self._get_user_vector_measure(user_name, champ_history, champ_idx, self.mode, 2)
 
         return mastery_score_vector
 
@@ -96,34 +128,26 @@ class UserVectorGenerator():
         return user_vectors
 
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Please specify the mode!')
+        sys.exit()
+
     user_vectors = {}
     mode = int(sys.argv[1])
 
-    if mode == 2: # TF-IDF mode
+    if mode == 2 or mode == 3: # TF-IDF mode or TF-IDF with global win rate mode
         idf_table = make_idf_table()
 
-    # batch for userlist 2
-    for i in range(12):
-        batch_file = './data_batch/batch2_' + str(i) + '.json'
-        user_vector_generator = UserVectorGenerator(batch_file, mode)
-        user_vectors.update(user_vector_generator.make_user_vectors())
-
-    # batch for userlist 3
-    for i in range(4):
-        batch_file = './data_batch/batch3_' + str(i) + '.json'
-        user_vector_generator = UserVectorGenerator(batch_file, mode)
-        user_vectors.update(user_vector_generator.make_user_vectors())
-
-    # batch for userlist 4
-    for i in range(3):
-        batch_file = './data_batch/batch4_' + str(i) + '.json'
-        user_vector_generator = UserVectorGenerator(batch_file, mode)
-        user_vectors.update(user_vector_generator.make_user_vectors())
+    batch_file = './data_batch/userbatch.json'
+    user_vector_generator = UserVectorGenerator(batch_file, mode)
+    user_vectors = user_vector_generator.make_user_vectors()
 
     if mode == 1:
-        user_vectors_file = './datasets/user_vectors_naive.json'
+        user_vectors_file = './datasets/user_vectors_naive2.json'
     elif mode == 2:
-        user_vectors_file = './datasets/user_vectors_tf_idf.json'
+        user_vectors_file = './datasets/user_vectors_tf_idf2.json'
+    elif mode == 3:
+        user_vectors_file = './datasets/user_vectors_tf_idf_global_win_rate.json'
 
     with open(user_vectors_file, 'w') as fp:
         json.dump(user_vectors, fp)
